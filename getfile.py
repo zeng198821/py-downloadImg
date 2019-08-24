09f1c8ba89a8b8db0707d19880011bf681cb8547
zeng <zeng198821@126.com>
2019-08-24 10:49:41 +0800

    整理代码

--- getfile.py
+++ getfile.py
@@ -63,7 +63,7 @@ def gDownloadWithFilename(url,savePath,file_para):
     #参数检查，现忽略
     try:
         headers = {
-            'Accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
             'Accept-Encoding':'gzip, deflate, sdch',
             'Accept-Language':'zh-CN,zh;q=0.8',
             'Cache-Control':'max-age=0',
